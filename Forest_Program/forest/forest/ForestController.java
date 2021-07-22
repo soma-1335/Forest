24aac6db1062b2a7d834a15589b4b2b58509513e
jundel88 <85608391+jundel88@users.noreply.github.com>
2021-07-22 13:44:38 +0900

    controller_sasaki
    forestcontroller作成ver1
    mouse関連記入

--- Forest_Program/forest/forest/ForestController.java
+++ Forest_Program/forest/forest/ForestController.java
@@ -1,5 +1,8 @@
 package forest;
 
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 
 public class ForestController extends eventModel {
@@ -8,36 +11,52 @@ public class ForestController extends eventModel {
 
 	private ForestModel forestModel;
 
-	public void ForestController() {
-
+	public void ForestController(ForestModel forestModel) {
+		this.forestModel = forestModel;
 	}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
+	public void mouseEntered(MouseEvent aMouseEvent) {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+	@Override
+	public void mouseExited(MouseEvent aMouseEvent) {
+		return;
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent aMouseEvent) {
+		// TODO Auto-generated method stub
+		Cursor aCursor = Cursor.getDefaultCursor();
+		Component aComponent = (Component)aMouseEvent.getSource();
+		aComponent.setCursor(aCursor);
+		current = aMouseEvent.getPoint();
+		previous = current;
+		return;
+		
+	}	
+
+	@Override
+	public void mouseClicked(MouseEvent aMouseEvent) {
+		// TODO Auto-generated method stub
+		Point aPoint = aMouseEvent.getPoint();
+		aPoint.translate(view.scrollAmount().x, view.scrollAmount().y);
+		System.out.println(aPoint);
+		return;
 		
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
+	public void mousePressed(MouseEvent aMouseEvent) {
 		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
+		Cursor aCursor = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
+		Component aComponent = (Component)aMouseEvent.getSource();
+		aComponent.setCursor(aCursor);
+		current = aMouseEvent.getPoint();
+		previous = current;
+		return;
 		
 	}
 
